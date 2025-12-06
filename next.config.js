dda04c17a39b5461675f72c73e55d7c07038da50
rogergrubb <roger@grubb.net>
2025-12-06 07:08:22 -0800

    Add webpack alias for Next.js 15 path resolution

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -16,6 +18,10 @@ const nextConfig = {
         hostname: '**.cloudinary.com',
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 };
 
