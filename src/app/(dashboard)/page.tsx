69c0f617f23937d0d39a8d984c5d792c3a497bdd
rogergrubb <roger@grubb.net>
2025-12-06 18:21:56 -0800

    Remove dashboard page to fix Next.js 15 route group build bug

--- src/app/(dashboard)/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function DashboardPage() {
-  // Redirect to books page - temporary fix for Next.js 15 build issue
-  redirect('/books');
-}
