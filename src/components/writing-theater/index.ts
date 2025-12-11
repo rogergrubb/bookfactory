b56c7a8608ebd7f4366b2fa11e042a90f2352194
rogergrubb <roger@grubb.net>
2025-12-11 08:18:34 -0800

    feat(writing-theater): Add index exports

---
+++ src/components/writing-theater/index.ts
@@ -0,0 +1,7 @@
+export { WritingTheater } from './WritingTheater';
+export { ToolTray } from './ToolTray';
+export { ToolPanel } from './ToolPanel';
+export { UndoStack } from './UndoStack';
+export type { UndoItem } from './UndoStack';
+export { EditorCanvas } from './EditorCanvas';
+export { ReferencePanel } from './ReferencePanel';
