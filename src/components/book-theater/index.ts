038ff1cd062d4af33bd5f589597d9dd1e9dc01df
rogergrubb <roger@grubb.net>
2025-12-12 00:30:45 -0800

    feat(theater): Add index.ts

---
+++ src/components/book-theater/index.ts
@@ -0,0 +1,13 @@
+// Book Theater Components
+export { ToolTray } from './ToolTray';
+export { ChapterTimeline } from './ChapterTimeline';
+export { WritingCanvas } from './WritingCanvas';
+export { ToolPanel } from './ToolPanel';
+export { UndoStack } from './UndoStack';
+export { SceneContextPanel } from './SceneContextPanel';
+
+// Types
+export * from './types';
+
+// Tool Definitions
+export { tools, toolsByCategory, categoryMeta, getToolById } from './tool-definitions';
