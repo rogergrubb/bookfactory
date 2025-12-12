2c1dcb47da268d9b0f72ac09129ce436451cfdbb
rogergrubb <roger@grubb.net>
2025-12-12 02:28:33 -0800

    feat(theater): Add index.ts - micro-interactions & command palette

--- src/components/book-theater/index.ts
+++ src/components/book-theater/index.ts
@@ -1,10 +1,36 @@
+// Components
 export { WritingCanvas } from './WritingCanvas';
 export { ToolTray } from './ToolTray';
 export { ToolPanel } from './ToolPanel';
 export { ChapterTimeline } from './ChapterTimeline';
 export { UndoStack } from './UndoStack';
 export { SceneContextPanel } from './SceneContextPanel';
+export { CommandPalette } from './CommandPalette';
+export { GenerationProgressBar, InlineProgress, FullScreenProgress } from './GenerationProgress';
+export { InsertPopup, ConfettiOverlay, TextHighlight, WordCountDelta, PulseRing, TypingCursor, SuccessCheck } from './InsertAnimations';
+
+// Hooks
+export {
+  useGenerationProgress,
+  useInsertAnimation,
+  useSuccessCelebration,
+  useTypewriter,
+  useFeedback,
+  useWordCountAnimation,
+} from './hooks/useMicroInteractions';
+
+// Types from hooks
+export type {
+  GenerationPhase,
+  GenerationProgress,
+  InsertAnimation,
+  Confetti,
+} from './hooks/useMicroInteractions';
+
+// Tool definitions
 export { tools, categoryMeta, getToolsByCategory, getToolById } from './tool-definitions';
+
+// Types
 export type {
   Book,
   Chapter,
