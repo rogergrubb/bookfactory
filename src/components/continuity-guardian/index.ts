8c451dadc2efa485da21d3d45e9cf39c4e4867c8
rogergrubb <roger@grubb.net>
2025-12-13 03:33:52 -0800

    feat(continuity): Export all components including badges and highlighters

--- src/components/continuity-guardian/index.ts
+++ src/components/continuity-guardian/index.ts
@@ -1,7 +1,9 @@
-// Continuity Guardian - Editor Integration Components
+// Continuity Guardian - All Components
 
 export * from './types';
 export { ContinuityDashboard } from './ContinuityDashboard';
 export { ContinuityIndicator, InlineIssueMarker } from './ContinuityIndicator';
 export { SelectionChecker } from './SelectionChecker';
 export { ContinuityToolPanel } from './ContinuityToolPanel';
+export { ChapterIssuesBadge, ChapterIssuesDot, useChapterIssuesBulk } from './ChapterIssuesBadge';
+export { InlineIssueHighlighter, useIssueHighlights } from './InlineIssueHighlighter';
